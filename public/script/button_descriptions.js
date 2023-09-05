e57b3ef0adb68edc058bb481c1de8ec7a5d0cf3f
Floha <floha2011@hotmail.de>
2023-09-05 23:54:02 +0200

    initial commit base working tracker

---
+++ public/script/button_descriptions.js
@@ -0,0 +1,164 @@
+var items = {
+    "gomode": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "instrument1": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "instrument2": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "instrument3": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "instrument4": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "instrument5": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "instrument6": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "instrument7": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "instrument8": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "sword": {
+        "type": "cycle",
+        "size": "majoritem",
+        "opts": ["sword0", "sword1", "sword2"],
+        "loop": false,
+        "disable_zero": true,
+    },
+    "feather": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "bracelet": {
+        "type": "cycle",
+        "size": "majoritem",
+        "opts": ["bracelet0", "bracelet1", "bracelet2"],
+        "loop": false,
+        "disable_zero": true,
+    },
+    "boots": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "flippers": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "hookshot": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "shield": {
+        "type": "cycle",
+        "size": "majoritem",
+        "opts": ["shield0", "shield1", "shield2"],
+        "loop": false,
+        "disable_zero": true,
+    },
+    "rod": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "shovel": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "bow": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "bombs": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "leafs": {
+        "type": "counter",
+        "size": "majoritem",
+        "minimum": 0,
+        "maximum": 5,
+    },
+    "tailkey": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "anglerkey": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "facekey": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "birdkey": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "slimekey": {
+        "type": "toggle",
+        "size": "majoritem"
+    },
+    "trade": {
+        "type": "cycle",
+        "size": "majoritem",
+        "opts": ["trade0", "trade1", "trade2", "trade3", "trade4", "trade5", "trade6", "trade7", "trade8", "trade9", "trade10", "trade11", "trade12", "trade13", "trade14"],
+        "disable_zero": true,
+    },
+    "boomerang": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "powder": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "ocarina": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "ballad": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "mambo": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+    "song": {
+        "type": "toggle",
+        "size": "majoritem",
+    },
+}
+
+/* templates
+Badge Template:
+"magic_lens": {
+        "type": "badge",
+        "size": "majoritem",
+        "opts": ["magic", "lensbadge"],
+},
+Split Template:
+"spells": {
+        "type": "split",
+        "size": "majoritem",
+        "opts": ["nospells", "dins", "farores", "dinsfarores"],
+        "disable_zero": true,
+        "loop": false,
+    },
+ */
